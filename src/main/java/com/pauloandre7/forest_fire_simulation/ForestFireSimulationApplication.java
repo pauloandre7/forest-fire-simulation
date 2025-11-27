789fbe860c30ad2249e9494b7e5183640f5e618a
pauloandre7 <paulos.2001@alunos.utfpr.edu.br>
2025-11-27 01:27:38 -0300

    chore: initial spring boot project setup

---
+++ src/main/java/com/pauloandre7/forest_fire_simulation/ForestFireSimulationApplication.java
@@ -0,0 +1,13 @@
+package com.pauloandre7.forest_fire_simulation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForestFireSimulationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForestFireSimulationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pauloandre7/forest_fire_simulation/ForestFireSimulationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pauloandre7.forest_fire_simulation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForestFireSimulationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
