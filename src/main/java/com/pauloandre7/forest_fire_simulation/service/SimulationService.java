e7bb712e845118329f651a00e978565b51445248
pauloandre7 <paulos.2001@alunos.utfpr.edu.br>
2025-12-03 01:02:17 -0300

    feat: adds calculateNextGeneration() with threads
    This method is used to implements the parallel mode to scan the matrix
    and apply new cell states using the automatum celullar automata logic.
    
    To solve this problem, the method use this steps:
    
    1. **SimulationTask: ** creates and store instances of SimulationTask in a
    List. Each task receives the range of index to work and two Forests:
    buffer to write and original to read.
    
    2. **Executor: ** With executor, the method invokeAll uses the threads to
    get tasks from the list and solve.
    
    3. **BufferForest instance:** as final step, the currentForest will
    receive the instance of BufferForest to store the new grid for another cycle.

--- src/main/java/com/pauloandre7/forest_fire_simulation/service/SimulationService.java
+++ src/main/java/com/pauloandre7/forest_fire_simulation/service/SimulationService.java
@@ -4,22 +4,33 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 import org.springframework.stereotype.Service;
 
 import com.pauloandre7.forest_fire_simulation.model.Cell;
 import com.pauloandre7.forest_fire_simulation.model.CellState;
+import com.pauloandre7.forest_fire_simulation.model.Direction;
 import com.pauloandre7.forest_fire_simulation.model.Forest;
-import com.pauloandre7.forest_fire_simulation.model.WindDirection;
+import com.pauloandre7.forest_fire_simulation.parallel.SimulationTask;
 
 @Service
 public class SimulationService {
-    
-    private final double BASE_BURNING_PROBABILITY = 0.4;
+    // one cell has 8 neighbors. each neighbor burning will increase base Prob. in 0.12
+    private final double BASE_BURNING_PROBABILITY = 0.125;
     private Forest currentForest;
-    
+    private final ExecutorService executor;
+
     public SimulationService(Forest currentForest){
         this.currentForest = currentForest;
+        
+        // get the amount of avaliable threads and creates a pool for them
+        int numberOfThreads = Runtime.getRuntime().availableProcessors();
+        executor = Executors.newFixedThreadPool(numberOfThreads);
     }
 
     public void generateRandomForest(int height, int width, int burningTime){
@@ -52,16 +63,50 @@ public class SimulationService {
         // now the fire starting point will be setted using random index.
         forestCells.get(random.nextInt(height)).get(random.nextInt(width)).startBurning(burningTime);
 
-        List<WindDirection> windDirections = new ArrayList<>(Arrays.asList(WindDirection.values()));
-        WindDirection randomWindDirection = windDirections.get(random.nextInt(windDirections.size()));
+        List<Direction> windDirections = new ArrayList<>(Arrays.asList(Direction.values()));
+        Direction randomWindDirection = windDirections.get(random.nextInt(windDirections.size()));
 
-        this.currentForest = new Forest(height, width, forestCells, randomWindDirection, random.nextDouble(1.0), BASE_BURNING_PROBABILITY);
+        this.currentForest = new Forest(height, width, forestCells, randomWindDirection, random.nextDouble(1.0), burningTime, BASE_BURNING_PROBABILITY);
     }
 
     public void initializeForest(int height, int width, List<List<Cell>> forestCells, 
-        WindDirection windDirection, double windSpeed){
+        Direction windDirection, double windSpeed, int burningTime){
         
         this.currentForest = new Forest(height, width, forestCells, windDirection, 
-            windSpeed, BASE_BURNING_PROBABILITY);
+            windSpeed, burningTime, BASE_BURNING_PROBABILITY);
+    }
+
+    @SuppressWarnings("CallToPrintStackTrace")
+    public void calculateNextGeneration(){
+        // Uses deep copy constructor to create a safe copy of currentForest
+        Forest bufferForest = new Forest(this.currentForest);
+
+        int halfOfCols = (this.currentForest.getCells().get(0).size()) / 2;
+        int finalCol = this.currentForest.getCells().get(0).size();
+
+        List<Callable<Forest>> taskList = new ArrayList<>();
+        
+        // Will organize the tasks ate half of each row.
+        for(int i = 0; i < this.currentForest.getCells().size(); i++){
+            
+            // creates 2 tasks for the 2 halfs of a row
+            SimulationTask taskHalfOfARow = new SimulationTask(this.currentForest, bufferForest, i, 0, halfOfCols);
+            SimulationTask taskLastOfARow = new SimulationTask(this.currentForest, bufferForest, i, halfOfCols, finalCol);
+            
+            taskList.add(taskHalfOfARow);
+            taskList.add(taskLastOfARow);
+            
+        }
+        
+        try{
+            // The code will stop here until the tasks be complete
+            List<Future<Forest>> results = executor.invokeAll(taskList);
+
+            for (Future<Forest> f : results) f.get();
+            
+            this.currentForest = new Forest(bufferForest);
+        } catch(InterruptedException | ExecutionException e){
+            e.printStackTrace();
+        }
     }
 }
