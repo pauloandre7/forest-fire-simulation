ad79c7909fb4b3e7c1e25905c331fd3d010741d1
pauloandre7 <paulos.2001@alunos.utfpr.edu.br>
2025-12-11 07:59:26 -0300

    chore: adds missing @EnableScheduling at main class
    This annotation is required at Spring framework to enable scheduler.

--- src/main/java/com/pauloandre7/forest_fire_simulation/ForestFireSimulationApplication.java
+++ src/main/java/com/pauloandre7/forest_fire_simulation/ForestFireSimulationApplication.java
@@ -2,7 +2,9 @@ package com.pauloandre7.forest_fire_simulation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class ForestFireSimulationApplication {
 
